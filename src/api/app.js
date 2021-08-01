c621fd7479fc71cbc2a392f4bde2a3dd778a3f43
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-01 14:30:35 -0300

    config initial

---
+++ src/api/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const { PORT } = process.env;
+
+app.use(
+  cors({
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/ping', (_req, res) => res.status(200).json('pong'));
+
+module.exports = app;
